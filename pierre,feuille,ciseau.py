fb4a1a45a0389a0d2cffe7c112e521e418216299
William-Bailleul <115979128+William-Bailleul@users.noreply.github.com>
2022-11-10 17:34:39 +0100

    Add files via upload

--- pierre,feuille,ciseau.py
+++ pierre,feuille,ciseau.py
@@ -1,4 +1,4 @@
-DEBUT
+#DEBUT
 #On admet une fonction random qui retourne un chiffre aléatoire entre 0 et 2
 #On admet une fonction input qui recupere le saisi d'un joueur lors de son execution
 #On définit une fonction PierreFeuilleCiseau qui prend en argument nombrePartiePourGagner(le nombre de round pour gagner la partie)(!!! un nombre impair !!!)
@@ -7,30 +7,30 @@ DEBUT
     #Tant que scoreOrdinateur OU scoreJoueur est différent de nombrePartiePourGagner
         #Alors on assigne a choixOrdinateur le retour de l'execution de la fonction random
         #Alors on assigne a choixJoueur le retour de l'execution de la fonction input (avec la question "feuille, pierre ou ciseau ?")
-        #Si le retour de l'execution de la fonction input est pierre
-            #Alors la variable prend
-        #OU SINON le retour de l'execution de la fonction input est feuille
-            #Alors
-        #OU SINON le retour de l'execution de la fonction input est ciseau
-            #Alors 
-        #Si la valeur de choixOrdinateur est différente à la valeur de choixJoueur
+        #Si le retour de l'execution de la fonction input est "pierre"
+            #Alors la variable choixJoueur prend la valeur 0
+        #OU SINON le retour de l'execution de la fonction input est "feuille"
+            #Alors la variable choixJoueur prend la valeur 1
+        #OU SINON le retour de l'execution de la fonction input est "ciseau"
+            #Alors la variable choixJoueur prend la valeur 2
+        #Si la valeur de choixOrdinateur est différente de valeur de choixJoueur
             #Si choixOrdinateur est égal à 0 
                 #Si choixJoueur est égal à 1
-                    #Alors ajouter 1 à scoreJoueur
+                    #Alors incrementer de 1 scoreJoueur
                 #Si choixJoueur est égal à 2
                     #Alors ajouter 1 à scoreOrdinateur
             #Si choixOrdinateur est égal à 1
                 #Si choixJoueur est égal à 0
-                    #Alors ajouter 1 à scoreOrdinateur
+                    #Alors incrementer de 1 scoreOrdinateur
                 #Si choixJoueur est égal à 2
-                    #Alors ajouter 1 à scoreJoueur
+                    #Alors incrementer de 1 scoreJoueur
             #Si choixOrdinateur est égal à 2 
                 #Si choixJoueur est égal à 0
-                    #Alors ajouter 1 à scoreJoueur
+                    #Alors incrementer de 1 scoreJoueur
                 #Si choixJoueur est égal à 1
-                    #Alors ajouter 1 à scoreOrdinateur
+                    #Alors incrementer de 1 scoreOrdinateur
     #Si scoreOrdinateur est égal nombrePartiePourGagner
         #Alors on afficher un message de défaite
     #Sinon
         #Alors on affiche un message de victoire
-FIN
+#FIN
